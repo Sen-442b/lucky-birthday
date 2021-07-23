525ab2ebec9b875b4a7406fc81ee475df6a2b246
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2021-07-23 23:44:23 +0530

    additional features added

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const date = document.querySelector('.date');
 const luckyNum = document.querySelector('.lucky-number');
 const submitBtn = document.querySelector('button');
+const result = document.querySelector('.result');
+const showImg = document.querySelector('.show-img');
+const  paragraph = document.getElementById('para');
+const modal = document.querySelector('.modal');
+modal.open()
 
-
-
+result.style.display = "none";
 submitBtn.addEventListener('click',()=>{
   let storeSum=0;
   let luckyNumValue = parseInt(luckyNum.value,10);
+  result.style.display="block";
+
+
  
   dateArray = date.value.split("-");
 
@@ -17,8 +24,20 @@ submitBtn.addEventListener('click',()=>{
       
 
   })
-
- if(storeSum %luckyNumValue ===0){
-   console.log("Lucky");
- }
+  getResultData(storeSum,luckyNumValue);
 })
+
+
+
+function getResultData(sum,luckyNumber)
+{
+ if(sum %luckyNumber ===0){
+  showImg.src="./images/happy_lucky.svg"
+  paragraph.innerText="You have a lucky birthday "
+
+ }else{
+   showImg.src="paragraph.innerText"
+   paragraph.innerText="Your don't have a lucky birthday"
+   
+ }
+}
