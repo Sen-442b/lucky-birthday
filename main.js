94abf2a08a2f4584761fa34e8ff4d039925a544a
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2021-07-24 23:29:39 +0530

    dialog box and basic styling added

--- main.js
+++ main.js
@@ -1,43 +1,45 @@
-const date = document.querySelector('.date');
-const luckyNum = document.querySelector('.lucky-number');
-const submitBtn = document.querySelector('button');
-const result = document.querySelector('.result');
-const showImg = document.querySelector('.show-img');
-const  paragraph = document.getElementById('para');
-const modal = document.querySelector('.modal');
-modal.open()
+const date = document.querySelector(".date");
+const luckyNum = document.querySelector(".lucky-number");
+const submitBtn = document.getElementById("btn-submit");
+const result = document.querySelector(".result");
+const showImg = document.querySelector(".show-img");
+const paragraph = document.getElementById("para");
+const modal = document.querySelector(".modal");
+const closeBtn = document.getElementById("close-btn");
 
 result.style.display = "none";
-submitBtn.addEventListener('click',()=>{
-  let storeSum=0;
-  let luckyNumValue = parseInt(luckyNum.value,10);
-  result.style.display="block";
+submitBtn.addEventListener("click", () => {
+  let storeSum = 0;
+  let luckyNumValue = parseInt(luckyNum.value, 10);
+  result.style.display = "block";
 
-
- 
   dateArray = date.value.split("-");
 
-  dateArray.forEach((element)=>{
-      let intEle =parseInt(element,10);
-      storeSum= storeSum+intEle
-    
-      
-
-  })
-  getResultData(storeSum,luckyNumValue);
-})
+  dateArray.forEach((element) => {
+    let intEle = parseInt(element, 10);
+    storeSum = storeSum + intEle;
+  });
+  getResultData(storeSum, luckyNumValue);
+});
 
 
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    modal.style.top = "10%";
+  }, 3000);
+});
+closeBtn.addEventListener("click", () => {
+  modal.style.top="-100%";
+});
 
-function getResultData(sum,luckyNumber)
-{
- if(sum %luckyNumber ===0){
-  showImg.src="./images/happy_lucky.svg"
-  paragraph.innerText="You have a lucky birthday "
 
- }else{
-   showImg.src="paragraph.innerText"
-   paragraph.innerText="Your don't have a lucky birthday"
-   
- }
+
+function getResultData(sum, luckyNumber) {
+  if (sum % luckyNumber === 0) {
+    showImg.src = "./images/happy_lucky.svg";
+    paragraph.innerText = "You have a lucky birthday ";
+  } else {
+    showImg.src = "paragraph.innerText";
+    paragraph.innerText = "Your don't have a lucky birthday";
+  }
 }
